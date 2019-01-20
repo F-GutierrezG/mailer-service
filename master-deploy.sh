550db0f975c96b244204af1320013fc08fdb43cf
Francisco Gutierrez <fgutierrez@grouponlatam.com>
2019-01-20 19:48:27 -0300

    Master deploy

---
+++ master-deploy.sh
@@ -0,0 +1,21 @@
+# !/bin/bash
+ssh -o StrictHostKeyChecking=no ubuntu@${PRODUCTION_SERVER} "docker login -u gitlab-ci-token -p $DOCKER_TOKEN registry.gitlab.com"
+
+ssh -o StrictHostKeyChecking=no ubuntu@${PRODUCTION_SERVER} 'docker network create --subnet=172.25.0.0/16 mailer-service-network'
+
+ssh -o StrictHostKeyChecking=no ubuntu@${PRODUCTION_SERVER} 'docker container stop mailer'
+ssh -o StrictHostKeyChecking=no ubuntu@${PRODUCTION_SERVER} 'docker container stop mailer-swagger'
+
+ssh -o StrictHostKeyChecking=no ubuntu@${PRODUCTION_SERVER} 'docker container rm mailer'
+ssh -o StrictHostKeyChecking=no ubuntu@${PRODUCTION_SERVER} 'docker container rm mailer-swagger'
+
+ssh -o StrictHostKeyChecking=no ubuntu@${PRODUCTION_SERVER} 'docker image rm $(docker images registry.gitlab.com/gusisoft/onelike/mailer-service/mailer-swagger -q)'
+ssh -o StrictHostKeyChecking=no ubuntu@${PRODUCTION_SERVER} 'docker image rm $(docker images registry.gitlab.com/gusisoft/onelike/mailer-service/mailer -q)'
+
+ssh -o StrictHostKeyChecking=no ubuntu@${PRODUCTION_SERVER} "docker run -d --restart always -e 'API_URL=definitions/swagger.yml' --name mailer-swagger --network mailer-service-network --ip 172.25.0.3 $REGISTRY_REPO/$SWAGGER:$TAG"
+ssh -o StrictHostKeyChecking=no ubuntu@${PRODUCTION_SERVER} "docker run -d --restart always -e 'FLASK_ENV=development' -e 'FLASK_APP=manage.py' -e 'APP_SETTINGS=project.config.DevelopmentConfig' -e 'SECRET_KEY=$PROD_SECRET_KEY' -e 'SENDGRID_API_KEY=$SENDGRID_API_KEY' --name mailer --network mailer-service-network --ip 172.25.0.2 $REGISTRY_REPO/$MAILER:$TAG"
+
+ssh -o StrictHostKeyChecking=no ubuntu@${PRODUCTION_SERVER} 'docker network connect onelike-network --ip 172.18.0.16 mailer'
+ssh -o StrictHostKeyChecking=no ubuntu@${PRODUCTION_SERVER} 'docker network connect onelike-network --ip 172.18.0.17 mailer-swagger'
+
+ssh -o StrictHostKeyChecking=no ubuntu@${PRODUCTION_SERVER} 'docker network connect users-service-network --ip 172.20.0.10 mailer'
--- stage-deploy.sh
+++ stage-deploy.sh
@@ -2,7 +2,6 @@
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker login -u gitlab-ci-token -p $DOCKER_TOKEN registry.gitlab.com"
 
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker network create --subnet=172.25.0.0/16 mailer-service-network'
-ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker network create --subnet=172.20.0.0/16 users-service-network'
 
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker container stop mailer'
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker container stop mailer-swagger'
@@ -13,7 +12,7 @@ ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker container rm mail
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker image rm $(docker images registry.gitlab.com/gusisoft/onelike/mailer-service/mailer-swagger -q)'
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker image rm $(docker images registry.gitlab.com/gusisoft/onelike/mailer-service/mailer -q)'
 
-ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker run -d --restart always -e 'API_URL=definitions/swagger.yml' --name mailer-swagger --network mailer-service-network --ip 172.20.0.3 $REGISTRY_REPO/$SWAGGER:$TAG"
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker run -d --restart always -e 'API_URL=definitions/swagger.yml' --name mailer-swagger --network mailer-service-network --ip 172.25.0.3 $REGISTRY_REPO/$SWAGGER:$TAG"
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker run -d --restart always -e 'FLASK_ENV=development' -e 'FLASK_APP=manage.py' -e 'APP_SETTINGS=project.config.DevelopmentConfig' -e 'SECRET_KEY=secret_key' -e 'SENDGRID_API_KEY=$SENDGRID_API_KEY' --name mailer --network mailer-service-network --ip 172.25.0.2 $REGISTRY_REPO/$MAILER:$TAG"
 
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker network connect onelike-network --ip 172.18.0.16 mailer'
