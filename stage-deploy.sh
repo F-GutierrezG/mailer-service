b374ff38d998776a0a2b8d36a7879dc09fdbdf88
Carlos Silva <anklosr@gmail.com>
2019-01-20 17:16:33 -0300

    Stage deploy

---
+++ stage-deploy.sh
@@ -0,0 +1,22 @@
+# !/bin/bash
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker login -u gitlab-ci-token -p $DOCKER_TOKEN registry.gitlab.com"
+
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker network create --subnet=172.25.0.0/16 mailer-service-network'
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker network create --subnet=172.20.0.0/16 users-service-network'
+
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker container stop mailer'
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker container stop mailer-swagger'
+
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker container rm mailer'
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker container rm mailer-swagger'
+
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker image rm $(docker images registry.gitlab.com/gusisoft/onelike/mailer-service/mailer-swagger -q)'
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker image rm $(docker images registry.gitlab.com/gusisoft/onelike/mailer-service/mailer -q)'
+
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker run -d --restart always -e 'API_URL=definitions/swagger.yml' --name mailer-swagger --network mailer-service-network --ip 172.20.0.3 $REGISTRY_REPO/$SWAGGER:$TAG"
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker run -d --restart always -e 'FLASK_ENV=development' -e 'FLASK_APP=manage.py' -e 'APP_SETTINGS=project.config.DevelopmentConfig' -e 'SECRET_KEY=secret_key' --name mailer --network mailer-service-network --ip 172.25.0.2 $REGISTRY_REPO/$MAILER:$TAG"
+
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker network connect onelike-network --ip 172.18.0.16 mailer'
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker network connect onelike-network --ip 172.18.0.17 mailer-swagger'
+
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker network connect users-service-network --ip 172.20.0.10 mailer'
