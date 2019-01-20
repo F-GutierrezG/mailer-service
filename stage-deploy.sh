f9e9dcb8bdbfccafbe536c4510cc7d87e9abae25
Carlos Silva <anklosr@gmail.com>
2019-01-20 17:27:59 -0300

    Stage deploy

--- stage-deploy.sh
+++ stage-deploy.sh
@@ -14,7 +14,7 @@ ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker image rm $(docker
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker image rm $(docker images registry.gitlab.com/gusisoft/onelike/mailer-service/mailer -q)'
 
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker run -d --restart always -e 'API_URL=definitions/swagger.yml' --name mailer-swagger --network mailer-service-network --ip 172.20.0.3 $REGISTRY_REPO/$SWAGGER:$TAG"
-ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker run -d --restart always -e 'FLASK_ENV=development' -e 'FLASK_APP=manage.py' -e 'APP_SETTINGS=project.config.DevelopmentConfig' -e 'SECRET_KEY=secret_key' --name mailer --network mailer-service-network --ip 172.25.0.2 $REGISTRY_REPO/$MAILER:$TAG"
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker run -d --restart always -e 'FLASK_ENV=development' -e 'FLASK_APP=manage.py' -e 'APP_SETTINGS=project.config.DevelopmentConfig' -e 'SECRET_KEY=secret_key' -e 'SENDGRID_API_KEY=$SENDGRID_API_KEY' --name mailer --network mailer-service-network --ip 172.25.0.2 $REGISTRY_REPO/$MAILER:$TAG"
 
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker network connect onelike-network --ip 172.18.0.16 mailer'
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker network connect onelike-network --ip 172.18.0.17 mailer-swagger'
