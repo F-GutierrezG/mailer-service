5805e5f7dfb0a1585f9363b6c0dd16a603ece9e5
Francisco Gutierrez <fgutierrez@grouponlatam.com>
2019-01-27 20:15:53 -0300

    Master deploy configuration

--- master-deploy.sh
+++ master-deploy.sh
@@ -13,7 +13,7 @@ ssh -o StrictHostKeyChecking=no ubuntu@${PRODUCTION_SERVER} 'docker image rm $(d
 ssh -o StrictHostKeyChecking=no ubuntu@${PRODUCTION_SERVER} 'docker image rm $(docker images registry.gitlab.com/gusisoft/onelike/mailer-service/mailer -q)'
 
 ssh -o StrictHostKeyChecking=no ubuntu@${PRODUCTION_SERVER} "docker run -d --restart always -e 'API_URL=definitions/swagger.yml' --name mailer-swagger --network mailer-service-network --ip 172.25.0.3 $REGISTRY_REPO/$SWAGGER:$TAG"
-ssh -o StrictHostKeyChecking=no ubuntu@${PRODUCTION_SERVER} "docker run -d --restart always -e 'FLASK_ENV=development' -e 'FLASK_APP=manage.py' -e 'APP_SETTINGS=project.config.DevelopmentConfig' -e 'SECRET_KEY=$PROD_SECRET_KEY' -e 'SENDGRID_API_KEY=$PROD_SENDGRID_API_KEY' --name mailer --network mailer-service-network --ip 172.25.0.2 $REGISTRY_REPO/$MAILER:$TAG"
+ssh -o StrictHostKeyChecking=no ubuntu@${PRODUCTION_SERVER} "docker run -d --restart always -e 'FLASK_ENV=production' -e 'FLASK_APP=manage.py' -e 'APP_SETTINGS=project.config.ProductionConfig' -e SECRET_KEY=$PROD_SECRET_KEY -e SENDGRID_API_KEY=$PROD_SENDGRID_API_KEY --name mailer --network mailer-service-network --ip 172.25.0.2 $REGISTRY_REPO/$MAILER:$TAG"
 
 ssh -o StrictHostKeyChecking=no ubuntu@${PRODUCTION_SERVER} 'docker network connect onelike-network --ip 172.18.0.16 mailer'
 ssh -o StrictHostKeyChecking=no ubuntu@${PRODUCTION_SERVER} 'docker network connect onelike-network --ip 172.18.0.17 mailer-swagger'
